38c21ace929cd23655a89327daa9a557b9735c8d
aesperer <piyrw9754@gmail.com>
2023-10-25 12:14:49 +0900

    provider, variable

---
+++ bitgouel-provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>4.0"
+      }
+    }    
+}
+
+
+provider "aws" {
+    region = "ap-northeast-2"
+    access_key = var.bitgouel_access_key
+    secret_key = var.gauth_secret_key
+}
