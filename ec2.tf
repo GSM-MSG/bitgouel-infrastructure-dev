c245443c8c4b63308bac9c4222155936ef3ad9f5
aesperer <piyrw9754@gmail.com>
2023-10-27 09:22:52 +0900

    ssm 정책 삭제

--- ec2.tf
+++ ec2.tf
@@ -6,26 +6,9 @@ resource "aws_instance" "bitgouel-main-server" {
   key_name = "bitgouel-key"
   associate_public_ip_address = false
   source_dest_check = false
-  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
     Name = "bitgouel-main-server"
   }
 
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "Fetching parameter from SSM"
-  my_param_value=$(aws ssm get-parameter --name MyParameter --query 'Parameter.Value' --output text)
-  echo "Received SSM parameter value: $my_param_value"
-  # 이제 SSM 파라미터 값을 사용하여 인스턴스 구성
-EOF
-
-}
-
-# SSM Parameters
-resource "aws_ssm_parameter" "my_param" {
-  name        = "MyParameter"
-  type        = "String"
-  value       = "MyValue"  # 원하는 값을 입력하세요.
-  description = "My SSM parameter description"
 }
