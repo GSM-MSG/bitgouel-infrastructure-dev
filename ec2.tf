3ad6c783ced3042a12f2ee691379e637dd32c44a
aesperer <piyrw9754@gmail.com>
2023-10-28 19:16:01 +0900

    bitgouel ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -9,8 +9,21 @@ resource "aws_instance" "bitgouel-main-server" {
   associate_public_ip_address = false
   source_dest_check = false
 
+
   tags = {
     Name = "bitgouel-main-server"
   }
 
+}
+
+resource "aws_instance" "bitgouel-bastion" {
+    ami = "ami-04cebc8d6c4f297a3"
+    instance_type = "t2.micro"
+    subnet_id =  "${aws_subnet.bitgouel-public-subnet-2a.id}"
+    vpc_security_group_ids = [aws_security_group.bitgouel-bastion-sg.id]
+    key_name = "bitgouel-key"
+
+    tags = {
+        Name = "bitgouel-bastion"
+    }
 }
